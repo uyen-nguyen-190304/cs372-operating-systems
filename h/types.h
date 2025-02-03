3d72f01a89720661d5662e99e89f883796b0f58c
Uyen Nguyen <nguyen_u1@denison.edu>
2025-02-02 23:54:59 -0500

    update types.h

--- h/types.h
+++ h/types.h
@@ -43,7 +43,7 @@ typedef struct semd_t {
 	struct semd_t	*s_next;			/* next element on the ASL */
 	int				*s_semAdd;			/* pointer to the semaphore */
 	pcb_t			*s_procQ;			/* tail pointer to a process queue */
-}
+} semd_t;
 
 /************************* DEVICE STRUCTURES *****************************/
 
