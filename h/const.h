59b6b8eb9958fe48e2c580d858c5919c9ad7e8e0
Uyen Nguyen <121514283+uyen-nguyen-190304@users.noreply.github.com>
2025-02-27 23:48:03 -0500

    update constants for phase 2

--- h/const.h
+++ h/const.h
@@ -3,124 +3,164 @@
 
 /**************************************************************************** 
  *
- * This header file contains utility constants & macro definitions.
+ * This header file contains utility constants and macro definitions used
+ * throughout the Pandos kernel. It includes:
+ *  - Hardware & software constants
+ *  - Processor status flags
+ *  - Exception codes
+ *  - Timer & memory constants
+ *  - SYSCALL code
+ *  - Device interrupt and register definition
+ *  - Helper macros for operations and memory alignment
  * 
  ****************************************************************************/
 
-/* Hardware & software constants */
-#define PAGESIZE		    4096			/* page size in bytes	*/
-#define WORDLEN			    4				/* word size in bytes	*/
 
 
-#define MAXDEVICES          49            /*  */
-#define NUCLEUSSTACKTOP     0x20001000    /* top of the nucleus stack */
+/******************************* Hardware & Software Constants *****************************/
 
-/* Processor Status - Status Register Constants */
-#define ALLOFF          0x0
-#define PLTON           0x08000000
-#define IMON            0x0000FF00 
-#define IEPON           0x00000004
-#define IECON           0x00000001
+#define PAGESIZE            4096			/* page size in bytes */
+#define WORDLEN             4				/* word size in bytes */
 
+#define MAXDEVICES          49              /* maximum number of external devices, plus additional semaphore for pseudo-clock */
+#define MAXPROC             20              /* Max concurrent processes supported */
 
-/* timer */
-#define TIMESLICE       5000                /* time slice in milliseconds */
-#define INFINITE        0xFFFFFFFF          /* infinite time */
+#define NUCLEUSSTACKTOP     0x20001000      /* top of the nucleus stack */
 
+/******************************* Processor Status Register Constants *****************************/
+
+#define ALLOFF              0x0                 /* all bits disabled */
+#define IECON               0x00000001          /* global interrupt enable */
+#define IECOFF              0xFFFFFFFE          /* global interrupt disable */
+#define IEPON               0x00000004          /* enable global interrupt after LDST */
+#define IMON                0x0000FF00          /* enable all interrupts */
+#define PLTON               0x08000000          /* Processor Local Timer enable */
+#define USERPON             0x00000008          /* set user-mode after LDST */
+
+/******************************* Timer Constants *****************************/
+
+#define TIMESLICE           5000                /* time slice in milliseconds (5ms) */
+#define INFINITE            0xFFFFFFFF          /* infinite time */
 
 /* timer, timescale, TOD-LO and other bus regs */
-#define RAMBASEADDR		0x10000000
-#define RAMBASESIZE		0x10000004
-#define TODLOADDR		0x1000001C
-#define INTERVALTMR		0x10000020	
-#define TIMESCALEADDR	0x10000024
+#define RAMBASEADDR		    0x10000000          /* start address of RAM */
+#define RAMBASESIZE		    0x10000004          /* size of RAM */
+#define TODLOADDR		    0x1000001C          /* Time of Day (TOD) clock */
+#define INTERVALTMR		    0x10000020	        /* interval timer */
+#define TIMESCALEADDR	    0x10000024          /* time scale factor */
+
+/******************************* Device Interrupt Constants *****************************/
+
+#define DISKINT			    3                   /* disk interrupt */
+#define FLASHINT 		    4                   /* flash memory interrupt */
+#define NETWINT 		    5                   /* network interface interrupt */
+#define PRNTINT 		    6                   /* printer interrupt */
+#define TERMINT			    7                   /* terminal interrupt */
+
+#define DEVINTNUM		    5		            /* interrupt lines used by devices */
+#define DEVPERINT		    8		            /* devices per interrupt line */
+#define DEVREGLEN		    4		            /* device register field length in bytes, and regs per dev */	
+#define DEVREGSIZE	        16 		            /* device register size in bytes */
+
+/******************************* Device Register Field Numbers *****************************/
+
+/* Non-terminal devices */
+#define STATUS			    0                   /* status register  */
+#define COMMAND			    1                   /* command register */
+#define DATA0			    2                   /* data register 0  */
+#define DATA1			    3                   /* data register 1  */
+
+/* Terminal devices */
+#define RECVSTATUS  	    0                   /* receiver status     */
+#define RECVCOMMAND 	    1                   /* receiver command    */
+#define TRANSTATUS  	    2                   /* transmitter status  */
+#define TRANCOMMAND 	    3                   /* transmitter command */
+
+/******************************* Device Common Status & Command Codes *****************************/
+
+/* device common STATUS codes */
+#define UNINSTALLED		    0                   /* device not installed */
+#define READY			    1                   /* device ready */
+#define BUSY			    3                   /* device busy */
+
+/* device common COMMAND code */
+#define RESET			    0                   /* reset device */
+#define ACK				    1                   /* acknowledge device operation */
+
+/******************************* Memory Constants *****************************/
+
+#define KSEG0               0x00000000          /* kernel segment 0 (uncached)*/
+#define KSEG1               0x20000000          /* kernel segment 1 (cached) */
+#define KSEG2               0x40000000          /* kernel segment 2 */
+#define KUSEG               0x80000000          /* user segment */
+
+#define RAMSTART            0x20000000          /* start of ram */
+#define BIOSDATAPAGE        0x0FFFF000          /* bios data page */
+#define	PASSUPVECTOR	    0x0FFFF900          /* pass up vector page */
+
+/******************************* Exception & Interrupt Constants *****************************/
+
+#define INTCONST            0                   /* interrupt exception code */
+#define TLBMIN              1                   /* minimum TLB exception code */
+#define TLBMAX              3                   /* maximum TLB exception code */
+#define SYSCALLCONST        8                   /* SYSCALL exception code */
+
+#define GETEXCEPTIONCODE    0xFF                /* mask for the exception code (bits 6..2) */
+#define CAUSESHIFT          2                   /* number of bits to shift right to get exception code */
+
+#define ERRORCONST          -1                  /* indicate an error occurred */
+#define SUCCESSCONST        0                   /* indicate success */
+
+/* Cause register constants for interrupt lines */
+#define LINE1INT            0x00000200          /* interrupt line 1 */
+#define LINE2INT            0x00000400          /* interrupt line 2 */
+#define LINE3INT            0x00000800          /* interrupt line 3 */
+#define LINE4INT            0x00001000          /* interrupt line 4 */
+#define LINE5INT            0x00002000          /* interrupt line 5 */
+#define LINE6INT            0x00004000          /* interrupt line 6 */
+#define LINE7INT            0x00008000          /* interrupt line 7 */
+
+/* Device number */
+#define DEV0                0x00000001         
 
 
-/* utility constants */
+/******************************* SYSCALL Constants *****************************/
+
+#define SYS1CALL            1                   /* create process */
+#define SYS2CALL            2                   /* terminate process */
+#define SYS3CALL            3                   /* passeren (semaphore wait) */
+#define SYS4CALL            4                   /* verhogen (semaphore signal) */
+#define SYS5CALL            5                   /* wait for IO */
+#define SYS6CALL            6                   /* get CPU time */
+#define SYS7CALL            7                   /* wait for clock */
+#define SYS8CALL            8                   /* get support data */
+
+/******************************* Exception Handling Constants *****************************/
+
+#define	PGFAULTEXCEPT	    0                   /* page fault exception */
+#define GENERALEXCEPT	    1                   /* general exception */
+
+/******************************* Utility Constants *****************************/
+
 #define	TRUE			    1
 #define	FALSE			    0
-#define HIDDEN			  static
-#define EOS				    '\0'
+#define HIDDEN			    static
+#define EOS                 '\0'
 
 #ifndef MAXINT
-#define MAXINT 		0xEFFFFFFF
+#define MAXINT 		        0xEFFFFFFF
 #endif
 
 #define NULL 			    ((void *)0xFFFFFFFF)
 
-/* Maximum number of processes */
-#define MAXPROC         20    /* Max concurrent processes supported */
+/******************************* Helper Macros *****************************/
 
-/* device interrupts */
-#define DISKINT			  3
-#define FLASHINT 		  4
-#define NETWINT 		  5
-#define PRNTINT 		  6
-#define TERMINT			  7
+/* Math operations */
+#define	MIN(A,B)		    ((A) < (B) ? A : B)
+#define MAX(A,B)		    ((A) < (B) ? B : A)
 
-#define DEVINTNUM		  5		  /* interrupt lines used by devices */
-#define DEVPERINT		  8		  /* devices per interrupt line */
-#define DEVREGLEN		  4		  /* device register field length in bytes, and regs per dev */	
-#define DEVREGSIZE	  16 		  /* device register size in bytes */
-
-/* device register field number for non-terminal devices */
-#define STATUS			  0
-#define COMMAND			  1
-#define DATA0			    2
-#define DATA1			    3
-
-/* device register field number for terminal devices */
-#define RECVSTATUS  	0
-#define RECVCOMMAND 	1
-#define TRANSTATUS  	2
-#define TRANCOMMAND 	3
-
-/* device common STATUS codes */
-#define UNINSTALLED		0
-#define READY			    1
-#define BUSY			    3
-
-/* device common COMMAND codes */
-#define RESET			    0
-#define ACK				    1
-
-/* Memory related constants */
-#define KSEG0             0x00000000
-#define KSEG1             0x20000000
-#define KSEG2             0x40000000
-#define KUSEG             0x80000000
-#define RAMSTART          0x20000000
-#define BIOSDATAPAGE      0x0FFFF000
-#define	PASSUPVECTOR	  0x0FFFF900
-
-/* General exception constants */
-#define INTCONST          0         /* Interrupt exception code */
-#define TLBMIN            1         /* Minimum TLB exception code */
-#define TLBMAX            3         /* Maximum TLB exception code */
-#define SYSCALLCONST      8         /* SYSCALL exception code */
-
-#define CAUSEMASK         0xFF      /* Mask for the exception code (bits 6..2) */
-#define CAUSESHIFT        2         /* Number of bits to shift right to get exception code */
-
-/* SYSCALL */
-#define SYS1CALL          1
-#define SYS2CALL          2
-#define SYS3CALL          3
-#define SYS4CALL          4
-#define SYS5CALL          5
-#define SYS6CALL          6
-#define SYS7CALL          7
-#define SYS8CALL          8
-
-/* Exceptions related constants */
-#define	PGFAULTEXCEPT	  0
-#define GENERALEXCEPT	  1
-
-
-/* operations */
-#define	MIN(A,B)		((A) < (B) ? A : B)
-#define MAX(A,B)		((A) < (B) ? B : A)
-#define	ALIGNED(A)		(((unsigned)A & 0x3) == 0)
+/* Memory alignment check */
+#define	ALIGNED(A)		    (((unsigned)A & 0x3) == 0)
 
 /* Macro to load the Interval Timer */
 #define LDIT(T)	((* ((cpu_t *) INTERVALTMR)) = (T) * (* ((cpu_t *) TIMESCALEADDR))) 
@@ -128,4 +168,4 @@
 /* Macro to read the TOD clock */
 #define STCK(T) ((T) = ((* ((cpu_t *) TODLOADDR)) / (* ((cpu_t *) TIMESCALEADDR))))
 
-#endif
+#endif  /* CONSTS */
