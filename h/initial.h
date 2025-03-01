db4191467431711404844d8190c39240db7062e6
Uyen Nguyen <121514283+uyen-nguyen-190304@users.noreply.github.com>
2025-02-28 23:00:59 -0500

    update documentation for initial.h

--- h/initial.h
+++ h/initial.h
@@ -1,16 +1,25 @@
 #ifndef INITIAL
 #define INITIAL
 
+/************************* INITIAL.h *****************************
+ *
+ * This header file declares global variables used for process management
+ * and device synchronization, ensuring that the following global variable
+ * are visible for any file that inlcude this header, while preventing
+ * multiple inclusions
+ *
+ * Written by   : Uyen Nguyen
+ * Last update  : 2025/02/28 
+ *
+ *****************************************************************/
 
 #include "../h/const.h"
 #include "../h/types.h"
 
-extern int processCount;
-extern int softBlockCount;
-extern pcb_PTR readyQueue;
-extern pcb_PTR currentProcess;
-extern int deviceSemaphores[MAXDEVICES];
+extern int processCount;                    /* Number of active processes in the system */
+extern int softBlockCount;                  /* Number of processes that are currently blocked */
+extern pcb_PTR readyQueue;                  /* Pointer to the queue of processes that are ready to run */
+extern pcb_PTR currentProcess;              /* Pointer to the currently executing process */
+extern int deviceSemaphores[MAXDEVICES];    /* Array of semaphores for device synchronization */
 
 #endif /* INITIAL */
-
-
