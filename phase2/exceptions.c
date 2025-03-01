011575004fab45c1ea74cdeb1d51d0bb2b2cc6fa
Uyen Nguyen <nguyen_u1@denison.edu>
2025-02-28 20:04:45 -0500

    Update exceptions.c

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -46,10 +46,10 @@ HIDDEN void getSupportData();
 
 
 void uTLB_RefillHandler() {
-    setENTRYHI(0x80000000);
-    setENTRYLO(0x00000000);
-    TLBWR();
-    LDST((state_PTR) 0x0FFFF000);
+	setENTRYHI(0x80000000);
+	setENTRYLO(0x00000000);
+	TLBWR();	
+	LDST ((state_PTR) 0x0FFFF000);
 }
 
 
