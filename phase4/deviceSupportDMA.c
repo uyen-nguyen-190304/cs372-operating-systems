f13e6077a2323763117cde23538f42528cfe3cec
Uyen Nguyen <nguyen_u1@denison.edu>
2025-05-07 00:48:24 -0400

    prototype for diskGet

--- phase4/deviceSupportDMA.c
+++ phase4/deviceSupportDMA.c
@@ -117,3 +117,81 @@ void diskPut(support_t *currentSupportStruct) {
 
 
 
+void diskGet(support_t *currentSupportStruct) {
+    /* Retrieve parameters from the except state */
+    int *logicalAddress = (int *) currentSupportStruct->sup_exceptState[GENERALEXCEPT].s_a1;
+    int diskNumber      = currentSupportStruct->sup_exceptState[GENERALEXCEPT].s_a2;
+    int sectionNumber   = currentSupportStruct->sup_exceptState[GENERALEXCEPT].s_a3;
+
+    /* Retrieve the three dimensional parameters */
+    devregarea_t *devRegArea = (devregarea_t *) RAMBASEADDR;
+    int maxCylinder = (devRegArea->devreg[diskNumber].d_data1) >> CYLINDERSHIFT;
+    int maxHead     = ((devRegArea->devreg[diskNumber].d_data1) & HEADMASK) >> HEADSHIFT ;
+    int maxSector   = (devRegArea->devreg[diskNumber].d_data1) & SECTORMASK;
+    int maxCount    = maxCylinder * maxHead * maxSector;
+
+    /* Defensive programming: check the virtual address and section number */
+    if ((sectionNumber < 0) || ((int) logicalAddress < KUSEG) || (sectionNumber < 0) || (sectionNumber > maxCount)) {
+        /* Terminate the U-proc */
+        SYSCALL(SYS9CALL, 0, 0, 0);
+    }
+
+    /* Gain mutual exclusion over the device's device register */
+    SYSCALL(SYS3CALL, (unsigned int) &devSemaphores[diskNumber], 0, 0);
+
+    /* Compute DMA buffer address */
+    memaddr *dmaBufferAddress = (memaddr *) (DISKSTART + (diskNumber * PAGESIZE));
+
+    /* Convert sector number to CHS */
+    int cylinder = sectionNumber / (maxHead * maxSector);
+    int head     = (sectionNumber / maxSector) % maxHead;
+    int sector   = sectionNumber % maxSector;
+ 
+    /* Disable interrupts for atomic operations: COMMAND + SYS5 */
+    setSTATUS(getSTATUS() & IECOFF);
+
+    /* Place the cylindar number and SEEK command in disk's COMMAND field */
+    devRegArea->devreg[diskNumber].d_command = (cylinder << CYLNUMSHIFT) | SEEKCYL;
+
+    /* Issue a SYS5 with the appropriate parameters */
+    int status = SYSCALL(SYS5CALL, DISKINT, diskNumber, FALSE);
+
+    /* Re-enable interrupts now that the atomic operation is complete */
+    setSTATUS(getSTATUS() | IECON);
+
+    /* If the seek device was successful, continue with the read operation */
+    if (status == SUCCESS) {
+        /* Write the starting address of the DMA buffer in the device's DATA0 field */
+        devRegArea->devreg[diskNumber].d_data0 = (unsigned int) dmaBufferAddress;
+
+        /* Disable interrupts for atomic operations: COMMAND + SYS5 */
+        setSTATUS(getSTATUS() & IECOFF);
+
+        /* Place the head number, section number, and READBLK command in disk's COMMAND field */
+        devRegArea->devreg[diskNumber].d_command = (head << HEADNUMSHIFT) | (sector << SECTORNUMSHIFT) | READBLK;
+
+        /* Issue a SYS5 with the appropriate parameters */
+        status = SYSCALL(SYS5CALL, DISKINT, diskNumber, FALSE);
+    
+        /* Re-enable interrupts now that the atomic operation is complete */
+        setSTATUS(getSTATUS() | IECON);
+    }
+
+    /* If any of the operation was unsuccessful */
+    if (status != SUCCESS) {
+        /* Set v0 to the negative of the status code to signal an error */
+        currentSupportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = -1 * status;
+    } else {
+        /* Copy the data from the device's DMA buffer -> U-proc's address space */
+        int i;
+        for (i = 0; i < PAGESIZE; i++) {
+            *(logicalAddress + i) = *(dmaBufferAddress + i);
+        }
+
+        /* Set v0 to the number of bytes read */
+        currentSupportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = status;
+    }
+
+    /* Release mutual exclusion over the device's device register */
+    SYSCALL(SYS4CALL, (unsigned int) &devSemaphores[diskNumber], 0, 0);
+}
